acfa28b30986234ac497a27b31292533e8ca3fd5
David Liberal <davidliberal0@gmail.com>
2025-12-28 14:04:45 -0500

    add loop to continuously ask user for input even if wrong

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,17 +13,20 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        LocalTime alarmTime;
+        LocalTime alarmTime = null;
 
-        try {
-            System.out.println("Enter an alarm time (HH:MM:SS): ");
-            String inputTime = scanner.nextLine();
+        while (alarmTime == null) {
+            try {
+                System.out.println("Enter an alarm time (HH:MM:SS): ");
+                String inputTime = scanner.nextLine();
 
-            alarmTime = LocalTime.parse(inputTime, formatter);
-            System.out.println("Alarm set for " + alarmTime);
-        } catch (DateTimeParseException e) {
-            System.out.println("Invalid format. Please use HH:MM:SS");
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                System.out.println("Alarm set for " + alarmTime);
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid format. Please use HH:MM:SS");
+            }
         }
+
 
 
 
