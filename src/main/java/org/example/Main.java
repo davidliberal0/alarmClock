5e8a38f5712c836d0b49fe26bfc9560243b787a0
David Liberal <davidliberal0@gmail.com>
2025-12-28 12:39:11 -0500

    set up time and time format objects and get time input from user

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,32 @@
 package org.example;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
+        // Alarm Clock
 
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime;
+
+        System.out.println("Enter an alarm time (HH:MM:SS): ");
+        String inputTime = scanner.nextLine();
+        
+
+
+
+
+
+
+
+
+
+
+        scanner.close();
     }
 }
