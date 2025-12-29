a5c2b85ee159ffb85eef2879a38b203bd75b9960
David Liberal <davidliberal0@gmail.com>
2025-12-28 21:23:44 -0500

    display and format local time to console

--- src/main/java/org/example/AlarmClock.java
+++ src/main/java/org/example/AlarmClock.java
@@ -13,14 +13,21 @@ public class AlarmClock implements Runnable {
 
     @Override
     public void run() {
-        LocalTime now = LocalTime.now();
-        while (now.isBefore(alarmTime)) {
+        while (LocalTime.now().isBefore(alarmTime)) {
             try {
                 Thread.sleep(1000);
+                LocalTime now = LocalTime.now();
+                System.out.printf("\r%02d:%02d:%02d",
+                        now.getHour(),
+                        now.getMinute(),
+                        now.getSecond());
+
             } catch (InterruptedException e) {
                 System.out.println("Thread was interrupted.");
                 throw new RuntimeException(e);
             }
         }
+
+        System.out.println("Alarm Noises ");
     }
 }
